05b53bc7b1db3c7487948d928623090453148a64
Arham <snehazahan@yahoo.com>
2025-12-02 16:31:07 -0500

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,3 +1,14 @@
+function openBrowser() {
+    alert("Browser app coming next! 🔥");
+}
+
+function openFiles() {
+    alert("File Manager coming next! 📁");
+}
+
+function openSettings() {
+    alert("Settings app coming next! ⚙️");
+}
 function openTerminal() {
     document.getElementById("terminalWindow").classList.remove("hidden");
     document.getElementById("terminalInput").focus();
